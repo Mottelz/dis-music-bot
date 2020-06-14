ac8321397e3047e4ddea58f563d2f5415927c8fb
Mottelz <shachna@gmail.com>
2020-06-14 13:21:56 -0400

    cleaned up code and added leave method

--- index.js
+++ index.js
@@ -11,24 +11,29 @@ client.on('message', async msg => {
     // If there's no guild, we can't go.
     if(!msg.guild) {
         msg.reply('No guild found.')
-    }
-    // If there's a guild and the user wants us to join, we try to join
-    else {
-        switch (msg.content){
+    } else {
+        // If we're in a guild, look for a command
+        switch (msg.content) {
+            // Join a voice channel if the user is already in a channel
             case '!join':
                 if(msg.member.voice.channel) {
-                    connection = await msg.member.voice.channel.join();
+                    connection = await msg.member.voice.channel.join()
                 } else {
-                    msg.reply('You need to join a voice channel first!');
+                    msg.reply('You need to join a voice channel first!')
                 }
                 break;
+            // Play audio in a channel that has been joined
             case '!play':
                 const dispatcher = connection.play(path.join(__dirname, 'music/underdog.mp3'))
                 dispatcher.on('finish', () => {
                     msg.reply('Finished playing')
-                    dispatcher.destroy();
+                    dispatcher.destroy()
                 })
                 break;
+            // Close the connection to whatever voice channel the bot is currently on.
+            case '!leave':
+                connection.disconnect()
+                break
         }
     }
 })
