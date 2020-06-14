077a436561915c2926ffad04adbd5cc07a22b73d
Mottelz <shachna@gmail.com>
2020-06-14 13:11:34 -0400

    Functioning version #1

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 by Mottel Zirkind
 
 This is a simple discord bot that plays audio files. It's built for teaching purposes. 
+
+### To Add Bot
+https://discordapp.com/oauth2/authorize?client_id=721762458410287124&scope=bot&permissions=8
--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // Package setup
 require('dotenv').config()
 const Discord = require('discord.js')
-const client = Discord.Client()
+const client = new Discord.Client()
+const path = require('path')
 
 // Launch the client
 client.login(process.env.token)
+
+client.on('message', async msg => {
+    // If there's no guild, we can't go.
+    if(!msg.guild) {
+        msg.reply('No guild found.')
+    }
+    // If there's a guild and the user wants us to join, we try to join
+    else {
+        switch (msg.content){
+            case '!join':
+                if(msg.member.voice.channel) {
+                    connection = await msg.member.voice.channel.join();
+                } else {
+                    msg.reply('You need to join a voice channel first!');
+                }
+                break;
+            case '!play':
+                const dispatcher = connection.play(path.join(__dirname, 'music/underdog.mp3'))
+                dispatcher.on('finish', () => {
+                    msg.reply('Finished playing')
+                    dispatcher.destroy();
+                })
+                break;
+        }
+    }
+})
